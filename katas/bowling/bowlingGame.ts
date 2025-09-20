4b441602a91e8637667c35414bca212771a4ce6c
Juanan Antona <juanantonio.antona@osapiens.com>
2025-09-20 08:33:36 +0200

    refactor(Bowling): Extracts methods within BowlingGame

--- katas/bowling/bowlingGame.ts
+++ katas/bowling/bowlingGame.ts
@@ -10,19 +10,32 @@ class BowlingGame {
   }
 
   processGameRolls(gameRolls: number[][]) {
-    gameRolls.forEach(([shotOne, shotTwo]) => {
-      if (this._isSpare) {
-        this._score += shotOne;
-        this._isSpare = false;
-      }
-      if (this._isStrike) {
-        this._score += shotOne + shotTwo;
-        this._isStrike = false;
-      }
-      this._score += shotOne + shotTwo;
-      this._isStrike = shotOne === 10;
-      this._isSpare = shotOne + shotTwo === 10 && shotTwo !== 0;
+    gameRolls.forEach((gameRoll) => {
+      this.processGameRoll(gameRoll);
     });
+  }
+
+  private processGameRoll(gameRoll: number[]) {
+    const [shotOne, shotTwo] = gameRoll;
+    if (this._isSpare) {
+      this._score += shotOne;
+      this._isSpare = false;
+    }
+    if (this._isStrike) {
+      this._score += shotOne + shotTwo;
+      this._isStrike = false;
+    }
+    this._score += shotOne + shotTwo;
+    this._isStrike = this.isStrike(shotOne);
+    this._isSpare = this.isSpare(shotOne, shotTwo);
+  }
+
+  private isStrike(shotOne: number): boolean {
+    return shotOne === 10;
+  }
+
+  private isSpare(shotOne: number, shotTwo: number): boolean {
+    return shotOne + shotTwo === 10 && shotTwo !== 0;
   }
 }
 
